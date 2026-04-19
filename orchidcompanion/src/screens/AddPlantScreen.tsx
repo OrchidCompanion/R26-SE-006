8d7bcfa1bb166bfb7ef8cc5b449fc594100cafca
Dinuka Rathnayake <174526798+sdinukarathnayake@users.noreply.github.com>
2026-04-20 01:56:44 +0530

    Added plant details and add plant screens

---
+++ orchidcompanion/src/screens/AddPlantScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddPlantScreen = () => {
+  return (
+    <View>
+      <Text>AddPlantScreen</Text>
+    </View>
+  )
+}
+
+export default AddPlantScreen
